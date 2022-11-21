ee6156c48f46924ce9879a7b730bfd70dfbd8c11
billu <billu@WMUREN-NB2>
2022-11-21 21:20:14 +0800

    added asp.net identities

--- Enrollment.Web/Controllers/HomeController.cs
+++ Enrollment.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Enrollment.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,6 +14,7 @@ namespace Enrollment.Web.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
--- Enrollment.Web/Program.cs
+++ Enrollment.Web/Program.cs
@@ -1,7 +1,18 @@
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Enrollment.Web.Data;
+using Enrollment.Web.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("EnrollmentWebContextConnection") ?? throw new InvalidOperationException("Connection string 'EnrollmentWebContextConnection' not found.");
+
+builder.Services.AddDbContext<EnrollmentWebContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<EnrollmentWebContext>();
 
 
 // Add services to the container.
@@ -19,11 +30,12 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
